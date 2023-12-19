1c8118b21e4752cfaa3f8dfebc4a60dc81581b01
Yuri Robert Censi <yurirobertcontato@gmail.com>
2023-12-19 22:02:28 +0000

    chore: initial website structuring

---
+++ content/blog/post.md
@@ -0,0 +1,7 @@
+----
+
+----
+
+# This is a Post
+
+This is a post. It is written in markdown and can be styled with **bold** or *italic* text.
--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,14 +9,32 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-mdx", {
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-mdx", 
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
-      "name": "pages",
-      "path": "./src/pages/"
+      path: './content/blog',
+      name: 'blog',
     },
-    __key: "pages"
-  }]
+    __key: 'blog',
+  },
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'images',
+      path: './src/images/',
+    },
+    __key: 'images',
+  },
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'pages',
+      path: './src/pages/',
+    },
+    __key: 'pages',
+  },
+]
 };
 
 export default config;
